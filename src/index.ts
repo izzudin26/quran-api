704fc3fd824da01b239555e42ae592ca554f0d2c
izzudin26 <azidarmor26@gmail.com>
2020-12-23 02:48:23 +0700

    add routes path

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import express, {Application, application} from "express"
+import bodyparser from "body-parser"
+
 const port = process.env.PORT || 8080;
 
 class App {
     constructor(
         public app: Application = express(),
-    ){}
+    ){
+        this.plugins()
+        this.routes()
+    }
 
     plugins(){
-
+        this.app.use(bodyparser.json())
     }
 
     routes(){
-
+        
     }
 }
 
